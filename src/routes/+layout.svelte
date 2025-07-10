23d148a6f3876831173bf7d888cae8a586f91fc1
18juba <jubalucas05@gmail.com>
2025-07-10 14:03:30 -0300

    estrutura inicial

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import '../app.css';
-	
+	import Fila from '$lib/components/layout/Fila.svelte';
+	import UltimosChamados from '$lib/components/layout/UltimosChamados.svelte';
+
 	let { children } = $props();
 </script>
 
 {@render children()}
+
+<Fila />
+<UltimosChamados />
+
