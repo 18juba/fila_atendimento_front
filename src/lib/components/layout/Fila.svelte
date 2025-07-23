cbf6415a2168c43d2d3eae1d2bf8b63674ed77e6
18juba <jubalucas05@gmail.com>
2025-07-23 14:11:34 -0300

    Ajustando scroll mobile e destacando o primeiro da fila

--- src/lib/components/layout/Fila.svelte
+++ src/lib/components/layout/Fila.svelte
@@ -2,7 +2,6 @@
   let fila = [
     { id: 11, nome: "Neymar" },
     { id: 18, nome: "Juan Pablo Vojvoda" },
-    // Adicione mais para testar o scroll
   ];
 </script>
 
@@ -13,13 +12,15 @@
   class="cursor-pointer"
 >
   <div
-    class="font1 fixed top-0 left-0 w-full md:w-36 md:left-6 md:top-2/12 h-24 md:h-7/12 bg-pink-800/80 flex rounded-none md:rounded-lg overflow-y-auto z-50"
+    class="font1 fixed top-0 left-0 w-full md:w-36 md:left-6 md:top-2/12 h-24 md:h-7/12 bg-pink-800/80 rounded-none md:rounded-lg overflow-y-auto z-50"
   >
     <ol
-      class="w-full font2 flex flex-col gap-1 justify-start text-slate-100 text-xl px-2 py-1"
+      class="w-full font2 text-slate-100 text-xl px-2 py-1"
     >
       {#each fila as pessoa}
-        <li class="w-full truncate italic"><span class=" font-bold">{pessoa.id}</span> {pessoa.nome}</li>
+        <li class="w-full truncate italic mb-0.5 last:mb-0 first:border-l-2 first:indent-1 first:bg-slate-100/20">
+          <span class="font-bold">{pessoa.id}</span> {pessoa.nome}
+        </li>
       {/each}
     </ol>
   </div>
