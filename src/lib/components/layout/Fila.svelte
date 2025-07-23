b163cdf60ac30207ef524bd510e22c8eb2cadc43
18juba <jubalucas05@gmail.com>
2025-07-23 13:36:25 -0300

    Responsividade mobile na fila

--- src/lib/components/layout/Fila.svelte
+++ src/lib/components/layout/Fila.svelte
@@ -1,13 +1,8 @@
 <script>
   let fila = [
-    {
-      id: 11,
-      nome: "Neymar",
-    },
-    {
-      id: 18,
-      nome: "Juan Pablo Vojvoda",
-    },
+    { id: 11, nome: "Neymar" },
+    { id: 18, nome: "Juan Pablo Vojvoda" },
+    // Adicione mais para testar o scroll
   ];
 </script>
 
@@ -18,13 +13,13 @@
   class="cursor-pointer"
 >
   <div
-    class="absolute p-2 top-2/12 left-6 w-36 h-7/12 bg-pink-800/80 flex rounded-lg overflow-hidden"
+    class="font1 fixed top-0 left-0 w-full md:w-36 md:left-6 md:top-2/12 h-24 md:h-7/12 bg-pink-800/80 flex rounded-none md:rounded-lg overflow-y-auto z-50"
   >
     <ol
-      class="w-full font2 flex flex-col gap-1 justify-start text-slate-100 text-sm truncate"
+      class="w-full font2 flex flex-col gap-1 justify-start text-slate-100 text-xl px-2 py-1"
     >
       {#each fila as pessoa}
-        <li>{pessoa.id}. {pessoa.nome}</li>
+        <li class="w-full truncate">{pessoa.id}. {pessoa.nome}</li>
       {/each}
     </ol>
   </div>
