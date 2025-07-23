6fe131327cab03c19a79b3b473a401b86f0ab60a
18juba <jubalucas05@gmail.com>
2025-07-23 13:36:10 -0300

    Mudando a font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import "@fontsource/smooch-sans";
+  import "@fontsource/alumni-sans-sc";
   import "../app.css";
   import Fila from "$lib/components/layout/Fila.svelte";
   import UltimosChamados from "$lib/components/layout/UltimosChamados.svelte";
